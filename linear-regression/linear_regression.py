d1ca202a5d53f1114ee829ddd9f83ba57c95c030
Lip0041 <1024682836@qq.com>
2021-07-08 17:16:34 +0800

    integrate the code of exp1

--- linear-regression/linear_regression.py
+++ linear-regression/linear_regression.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def process_data(data):
-    # 在第一列之前添加一列1
-    data.insert(0, 'Ones', 1)
+    data.insert(0, 'Ones', 1)   # 在第一列之前添加一列1
     cols = data.shape[1]
     X = data.iloc[:, 0: cols - 1]
     y = data.iloc[:, cols - 1: cols]
@@ -16,15 +15,15 @@ def process_data(data):
 
 
 def compute_cost(X, y, theta):
+    # 计算cost
     inner = np.power(((X * theta.T) - y), 2)
     return np.sum(inner) / (2 * len(X))
 
 
 def gradient_decent(X, y, theta, alpha, iteration):
+    # 梯度下降
     temp = np.matrix(np.zeros(theta.shape))
-    # ravel()方法，将多维数组转换为一维数组，然后用shape[1]获取列数
-    # parameters是参数的个数，即theta的最大下标
-    parameters = int(theta.ravel().shape[1])
+    parameters = int(theta.ravel().shape[1])    # ravel()方法，将多维数组转换为一维数组，然后用shape[1]获取列数
     cost = np.zeros(iteration)
     # 每一次迭代，更新theta
     for i in range(iteration):
@@ -45,12 +44,13 @@ def gradient_decent(X, y, theta, alpha, iteration):
     return theta, cost
 
 
-def draw_linear_model(data, g):
+def draw_model1(data, g):
     x = np.linspace(data.Population.min(), data.Population.max(), 100)
     f = g[0, 0] + (g[0, 1] * x)
-
+    # 画出线性拟合
     fig, ax = plt.subplots(figsize=(12, 8))
     ax.plot(x, f, 'r', label='Prediction')
+    # 画出原数据
     ax.scatter(data.Population, data.Profit, label='Training Data')
     ax.legend(loc=2)
     ax.set_xlabel('Population')
@@ -59,27 +59,30 @@ def draw_linear_model(data, g):
     plt.show()
 
 
-def exp1_1():
-    # 利用 pandas 读取数据
-    path = 'ex1data1.txt'
-    data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
-    # 提取其中的数据为matrix形式
-    X, y = process_data(data)
-    # 初始化 theta
-    theta = np.matrix(np.array([0, 0]))
-    # 学习率
-    alpha = 0.01
-    # 迭代次数
-    iteration = 1000
-    g, cost = gradient_decent(X, y, theta, alpha, iteration)
+def draw_model2(data, g):
+    x1 = np.linspace(data.Size.min(), data.Size.max())
+    x2 = np.linspace(data.Bedrooms.min(), data.Bedrooms.max())
+    x1, x2 = np.meshgrid(x1, x2)
+    h = g[0, 0] + g[0, 1] * x1 + g[0, 2] * x2
     # 画出线性拟合
-    draw_linear_model(data, g)
-    draw_3d(X, y)
-    print(normal_eqn(X, y))
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(x1, x2, h, cmap='rainbow')
+    # 画出原数据
+    a = data.iloc[:, 1: 2]
+    b = data.iloc[:, 2: 3]
+    c = data.iloc[:, 3: 4]
+    ax.scatter(a, b, c, c='r')
+    ax.set_xlabel('Size')
+    ax.set_ylabel('Bedrooms')
+    ax.set_zlabel('Price')
+    ax.set_title('Price vs. Size and Bedrooms')
+    plt.show()
 
 
 def draw_3d(X, y):
-    # 参考链接：https://www.pythonheidong.com/blog/article/576895/340066ed356e3e68be33/
+    # 参考链接：
+    # https://www.pythonheidong.com/blog/article/576895/340066ed356e3e68be33/
     J = np.zeros((100, 100))
     theta0 = np.linspace(-10, 10, 100)
     theta1 = np.linspace(-1, 4, 100)
@@ -94,52 +97,41 @@ def draw_3d(X, y):
     ax.set_ylabel('theta1')
     ax.set_zlabel('J')
     plt.show()
-    # contour
+    # contour，等高线绘制
     plt.figure()
-    contour = np.logspace(-3, 3, 20)
+    contour = np.logspace(-3, 3, 20)    # 以10**-3次方（0.001）和10**3（1000）之间的对数间隔绘制20条等高线
     z = plt.contour(theta0, theta1, J, levels=contour)
-    plt.clabel(z)
-    plt.show()
-
-
-def exp1_2():
-    path = 'ex1data2.txt'
-    data = pd.read_csv(path, header=None, names=['Size', 'Bedrooms', 'Price'])
-    # 归一化特征
-    # mean()获取平均值，std()获取标准差
-    data = (data - data.mean()) / data.std()
-    X, y = process_data(data)
-    theta = np.matrix(np.array([0, 0, 0]))
-    g, cost = gradient_decent(X, y, theta, alpha=0.01, iteration=1000)
-    draw_model(data, g)
-
-
-def draw_model(data, g):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    x1 = np.linspace(data.Size.min(), data.Size.max())
-    x2 = np.linspace(data.Bedrooms.min(), data.Bedrooms.max())
-    x1, x2 = np.meshgrid(x1, x2)
-    h = g[0, 0] + g[0, 1] * x1 + g[0, 2] * x2
-    # 画出线性拟合
-    ax.plot_surface(x1, x2, h, cmap='rainbow')
-    a = data.iloc[:, 1: 2]
-    b = data.iloc[:, 2: 3]
-    c = data.iloc[:, 3: 4]
-    ax.scatter(a, b, c, c='r')
+    plt.clabel(z)   # 显示每条等高线的代价函数值
     plt.show()
 
 
 def normal_eqn(X, y):
     # 正规方程求解theta
-    # X@T <--> X.dot(T)
-    theta = np.linalg.inv(X.T @ X) @ X.T @ y
+    # tip: X@T <--> X.dot(T)
+    theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
     return theta.T
 
 
 def main():
-    exp1_1()
-    # exp1_2()
+    # 单变量线性回归
+    path = 'ex1data1.txt'
+    data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
+    X, y = process_data(data)   # 提取其中的数据为matrix形式
+    theta = np.matrix(np.array([0, 0]))     # 初始化 theta
+    g, cost = gradient_decent(X, y, theta, alpha=0.01, iteration=1000)  # 梯度下降
+    draw_model1(data, g)    # 画出线性拟合
+    draw_3d(X, y)    # 绘制theta与J的关系
+    print(normal_eqn(X, y))    # 正规方程求解
+    # 多变量线性回归
+    path = 'ex1data2.txt'
+    data = pd.read_csv(path, header=None, names=['Size', 'Bedrooms', 'Price'])
+    data = (data - data.mean()) / data.std()    # 归一化特征，其中 mean()获取平均值，std()获取标准差
+    # 以下同上
+    X, y = process_data(data)
+    theta = np.matrix(np.array([0, 0, 0]))
+    g, cost = gradient_decent(X, y, theta, alpha=0.01, iteration=1000)
+    draw_model2(data, g)
+    print(normal_eqn(X, y))
 
 
 if __name__ == '__main__':
