f52ca765e4573d91bec25d87e3f793ea424243cb
Lip0041 <1024682836@qq.com>
2021-07-10 22:39:47 +0800

    update logistic_regression.py

--- logistic-regression/logistic_regression.py
+++ logistic-regression/logistic_regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import optimize
 
 
 def draw_data(data, x1, x2, y1, y2):
@@ -19,16 +20,6 @@ def draw_data(data, x1, x2, y1, y2):
     plt.show()
 
 
-def map_feature(x1, x2):
-    degree = 2
-    out = np.ones((x1.shape[0], 1))
-    for i in np.arange(1, degree + 1):
-        for j in range(i + 1):
-            temp = x1 ** (i - j) * (x2 ** j)
-            out = np.hstack((out, temp.reshape(-1, 1)))
-    return out
-
-
 def ex_data1():
     path = 'ex2data1.txt'
     data = pd.read_csv(path, header=None, names=['Exam1', 'Exam2', 'Admitted'])
@@ -41,7 +32,7 @@ def ex_data1():
     y = np.matrix(y.values)
     theta = np.matrix(np.zeros(X.shape[1]))
     # gradient descend
-    res = gradient(theta, X, y)
+    res = gradient_descent(theta, X, y)
     print(compute_cost(res, X, y))
     # 利用这个最小theta, 计算预测准确率
     print_prediction(res, X, y)
@@ -57,7 +48,7 @@ def compute_cost(theta, X, y):
     return np.mean(np.multiply(-y, np.log(h)) - np.multiply((1 - y), np.log(1 - h)))
 
 
-def gradient(theta, X, y, learningRate=1, iteration=100):
+def gradient_descent(theta, X, y, learningRate=1, iteration=100):
     cost = np.zeros(iteration)
     for i in range(iteration):
         error = sigmoid(X * theta.T) - y
@@ -79,31 +70,63 @@ def predict(theta, X):
     return [1 if x >= 0.5 else 0 for x in probability]
 
 
+def cost_regularized(theta, X, y, lambda0):
+    theta = np.matrix(theta)
+    h = sigmoid(X * theta.T)
+    reg = lambda0 / 2.0 / len(X) * np.mean(np.multiply(theta, theta))
+    return np.mean(np.multiply(-y, np.log(h)) - np.multiply((1 - y), np.log(1 - h))) + reg
+
+
+def gradient(theta, X, y, lambda0):
+    error = sigmoid(X * theta.T) - y
+    grad = (X.T * error) / len(X)
+    for i in range(theta.shape[1]):
+        if i == 0:
+            continue
+        else:
+            grad[i, 0] += theta[0, i] * lambda0 / len(X)
+    return grad.T
+
+
 def ex_data2():
     path = 'ex2data2.txt'
     data = pd.read_csv(path, header=None, names=['Test 1', 'Test 2', 'Accepted'])
-    draw_data(data, 'Test 1', 'Test 2', 'Accepted', 'Rejected')
-    degree = 5
-    x1 = data['Test 1']
-    x2 = data['Test 2']
+    # draw_data(data, 'Test 1', 'Test 2', 'Accepted', 'Rejected')
 
-    data.insert(3, 'Ones', 1)
-
-    for i in range(1, degree):
-        for j in range(0, i):
-            data['F' + str(i) + str(j)] = np.power(x1, i - j) * np.power(x2, j)
-
-    data.drop('Test 1', axis=1, inplace=True)
-    data.drop('Test 2', axis=1, inplace=True)
-
-    cols = data.shape[1]
-    x = np.array(data.iloc[:, 1: cols].values)
-    y = np.array(data.iloc[:, 0: 1].values)
-    theta = np.matrix(np.zeros(11))
-    compute_cost(theta, x, y)
-    res = gradient(theta, x, y, learningRate=1, iteration=1000)
+    data.insert(0, 'Ones', 1)
+    # data.iloc[:, 1: -1] = (data.iloc[:, 1: -1] - data.iloc[:, 1: -1].mean()) / data.iloc[:, 1: -1].std()
+    X = data.iloc[:, 0: -1]
+    y = data.iloc[:, -1:]
+    X = np.matrix(X.values)
+    y = np.matrix(y.values)
+    theta = np.matrix(np.zeros(X.shape[1]))
+    print(cost_regularized(theta, X, y, 0.1))
+    res = gradient(theta, X, y, 0.1)
     # print prediction
-    print_prediction(res, x, y)
+    result = optimize.fmin_bfgs(cost_regularized, theta, fprime=gradient, args=(X, y, 0.1))
+    print_prediction(result, X, y)
+
+    # degree = 5
+    # x1 = data['Test 1']
+    # x2 = data['Test 2']
+    #
+    # data.insert(3, 'Ones', 1)
+    #
+    # for i in range(1, degree):
+    #     for j in range(0, i):
+    #         data['F' + str(i) + str(j)] = np.power(x1, i - j) * np.power(x2, j)
+    #
+    # data.drop('Test 1', axis=1, inplace=True)
+    # data.drop('Test 2', axis=1, inplace=True)
+    #
+    # cols = data.shape[1]
+    # x = np.array(data.iloc[:, 1: cols].values)
+    # y = np.array(data.iloc[:, 0: 1].values)
+    # theta = np.matrix(np.zeros(11))
+    # print(compute_cost(theta, x, y))
+    # res = gradient(theta, x, y, 0.1)
+    # # print prediction
+    # print_prediction(res, x, y)
 
 
 def print_prediction(result, x, y):
@@ -115,5 +138,5 @@ def print_prediction(result, x, y):
 
 
 if __name__ == '__main__':
-    ex_data1()
+    # ex_data1()
     ex_data2()
